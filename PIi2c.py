aa56132d936e921f7188472730568618e2f82409
Bdawgdeep <36241121+Bdawgdeep@users.noreply.github.com>
2018-04-27 03:30:32 +0000

    Update PIi2c.py

--- PIi2c.py
+++ PIi2c.py
@@ -9,10 +9,17 @@ import smbus
 import time
 
 # for RPI version 1, use "bus = smbus.SMBus(0)"
-bus = smbus.SMBus(1)
+#bus = smbus.SMBus(1)
 
 # This is the address we setup in the Arduino Program
 #Slave Address 1
+#address = 0x04
+
+# for RPI version 1, use "bus = smbus.SMBus(0)"	
+bus = smbus.SMBus(1)
+
+	# This is the address we setup in the Arduino Program
+	#Slave Address 1
 address = 0x04
 
 
@@ -28,16 +35,17 @@ def readNumber():
     number = bus.read_i2c_block_data(address,0,16)
     print(number)
     return 1
-    
-while True:
-	#Receives the data from the User
-    data = raw_input("Enter the data to be sent : ")
-    data_list = list(data)
-    for i in data_list:
-    	#Sends to the Slaves 
-        writeNumber(int(ord(i)))
-        time.sleep(.1)
-        readNumber()
-    writeNumber(int(0x0A))
 
-#End of the Script
+def speakpi(data):  
+
+	while True:
+		#Receives the data from the User
+		#data = raw_input("Enter the data to be sent : ")
+		data_list = list(data)
+		for i in data_list:
+			#Sends to the Slaves 
+			writeNumber(int(ord(i)))
+			time.sleep(.1)
+			readNumber()
+		writeNumber(int(0x0A))
+	return True
